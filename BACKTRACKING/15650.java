75b3174f1d639c0bf754f9e8d2c2d9265e834255
gksrbgks2021 <gksrbgks2021@gmail.com>
2022-02-08 08:32:15 +0000

    n C m 조합

---
+++ BACKTRACKING/15650.java
@@ -0,0 +1,93 @@
+
+import java.util.*;
+
+import java.io.*;
+
+public class Main {
+	public static int[] map;
+	public static boolean visit[] = new boolean[9];
+	public static Set<ArrayList<Integer>> set = new HashSet<>();
+	public static ArrayList<Integer> list =new ArrayList<>();
+	public static void main(String[] args) throws IOException {
+		FastReader fr = new FastReader();
+		int a = fr.nextInt();// 총 개수
+		int b = fr.nextInt();// 뽑는 개수
+		int temp = a;
+		map = new int[b];
+		
+		helper(a,b,0);
+	}
+
+	public static void helper(int a, int b,int depth) {
+		if (b == depth) {
+			list.clear();
+			for(int i = 0 ;i < map.length; i++) {
+				list.add(map[i]);
+						}
+			Collections.sort(list);
+			if(!set.contains(list)) {
+				set.add(list);
+				for(int i : list) {
+					System.out.print(i+" ");
+				}
+				System.out.println();
+			}
+			return ;
+		}
+		
+		for (int i = 0; i < a; i++) {
+			if (visit[i])
+				continue;
+			visit[i] = true;
+			map[depth] = i+1;
+			helper( a, b ,depth+1  );
+			visit[i] = false;
+		}
+	}
+
+	public static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		public FastReader(String s) throws FileNotFoundException {
+			br = new BufferedReader(new FileReader(new File(s)));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
